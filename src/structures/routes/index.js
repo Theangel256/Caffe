3fb4785ff3ca8b932955cec4fb421f6a8f7d78e4
Theangel256 <amarillasgarcialuisangel7@gmail.com>
2020-11-17 20:06:01 -0700

    mantenimiento

--- src/structures/routes/index.js
+++ src/structures/routes/index.js
@@ -21,13 +21,14 @@ router.get('/', (req, res) => {
 .post('/github', (req, res) => {
 		if(req.method === "POST") {
 			if(req.headers["x-github-event"] === "push") {
+				res.sendStatus(204)
 				try {
+					console.log("Actualizacion Encontrada")
 					console.log(execSync("cd /home/pruebas/Caffe && git pull && pm2 restart default").toString());
 				} catch (e) {
 					console.error(e);
 				}
 			}
 		}
-		res.sendStatus(204)
 })
 module.exports = router;
