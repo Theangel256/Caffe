976f1b1cc14c9ac9bb09007fff7032388c9709de
Theangel256 <amarillasgarcialuisangel7@gmail.com>
2020-11-17 20:26:25 -0700

    mantenimiento

--- src/structures/routes/dashboard.js
+++ src/structures/routes/dashboard.js
@@ -12,7 +12,7 @@ router.get('/', auth, async (req, res) => {
 		guilds,
 		user: req.user,
 		clientAvatarURL: req.bot.user.displayAvatarURL({format: "jpg"}),
-		userAvatarURL: req.bot.users.fetch(req.user.id).displayAvatarURL({format:"jpg"}),
+		userAvatarURL: req.bot.users.resolve(req.user.id).displayAvatarURL({dynamic: true}),
 		client: req.bot,
 	});
 })
