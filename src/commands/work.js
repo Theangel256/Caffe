d95e2171c19a7771375ef588c2bf5d00c6aaab2f
Theangel256 <amarillasgarcialuisangel7@gmail.com>
2020-11-29 17:33:32 -0700

    work fixed

--- src/commands/work.js
+++ src/commands/work.js
@@ -1,7 +1,7 @@
 const economy = require('../structures/models/systemEconomy');
+const { getData } = require('../structures/functions/databaseManager');
 module.exports.run = async (client, message) => {
-	const user = message.author.id
-	const moneyUserServer = await economy.findOne({ userID: user});
+	const moneyUserServer = await getData({guildID: message.guild.id, userID: message.author.id}, 'systemEconomy')
 	let elements = ['100','75', '50'];
 	let cacthElements = elements[Math.floor(elements.length * Math.random())];
 	let jobs = ['Trabajas en una cafeteria y ganaste $', 'Trabajaste de repartidor de pizas y recibiste $', "Editaste un video de un YouTuber y te pago $"]
