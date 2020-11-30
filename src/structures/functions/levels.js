9ab68695de2c229534a257c808a934d83b237497
Theangel256 <amarillasgarcialuisangel7@gmail.com>
2020-11-29 19:45:11 -0700

    levels.js fixed

--- src/structures/functions/levels.js
+++ src/structures/functions/levels.js
@@ -1,21 +1,20 @@
-const SystemLvl = require('../models/SystemLvl');
+const { getData, updateData } = require('./databaseManager');
 const cooldownniveles = new Map();
-const set = require('./set')
 module.exports = async function (message) {
     if(cooldownniveles.has(message.guild.id + message.author.id)) {
         const time = cooldownniveles.get(message.guild.id + message.author.id);
         if(Date.now() < time) return;
     }
-    let niveles = await SystemLvl.findOne({ guildID: message.guild.id, userID: message.author.id});
-    if(!niveles) niveles = await set(SystemLvl, { xp: 0, lvl: 1, guildID: message.guild.id}) 
+    let niveles = await getData({guildID: message.guild.id, userID: message.author.id}, 'SystemLvl');
+    if(!niveles) await updateData({guildID: message.guild.id, userID: message.author.id}, {xp: 0, lvl: 1}, 'SystemLvl');
     const randomxp = Math.ceil(Math.random() * 10);
     const lvlup = niveles.lvl * 80;
     cooldownniveles.set(message.guild.id + message.author.id, Date.now() + 5000);
     if((niveles.xp + randomxp) >= lvlup) {
-        await SystemLvl.updateOne({ guildID: message.guild.id, userID: message.author.id}, {$set: { xp: 0, lvl: parseInt(niveles.lvl + 1)}}) 
+        await updateData({guildID: message.guild.id, userID: message.author.id}, { xp: 0, lvl: parseInt(niveles.lvl+ 1)}, 'SystemLvl');
         return message.channel.send(`Felicidades ${message.author.tag}, Subiste al nivel ${parseInt(niveles.lvl + 1)}!`);
     } else {
-        await SystemLvl.updateOne({ guildID: message.guild.id, userID: message.author.id}, {$inc: { xp: randomxp}});
+        await updateData({guildID: message.guild.id, userID: message.author.id}, {$inc: { xp: randomxp}}, 'SystemLvl');
         return;
     }
 }
