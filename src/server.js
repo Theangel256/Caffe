a1e722b5174b2116232439dbd20978b4f832c66d
Theangel256 <angel.amarillas@proton.me>
2025-05-13 19:50:22 -0700

    Refactor server.js to improve error handling and ensure consistent client login process

--- src/server.js
+++ src/server.js
@@ -1,31 +1,41 @@
 const { Client, Collection, GatewayIntentBits, Partials } = require('discord.js');
 const dbConnect = require('./utils/db.js');
-const client = new Client({
+
+async function start() {
+  const client = new Client({
     intents: [ 
-        GatewayIntentBits.Guilds,
-        GatewayIntentBits.GuildModeration,
-        GatewayIntentBits.GuildMessages,
-        GatewayIntentBits.GuildMessageReactions,
-        GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.GuildPresences,
-        GatewayIntentBits.MessageContent,
+      GatewayIntentBits.Guilds,
+      GatewayIntentBits.GuildModeration,
+      GatewayIntentBits.GuildMessages,
+      GatewayIntentBits.GuildMessageReactions,
+      GatewayIntentBits.GuildMembers,
+      GatewayIntentBits.GuildPresences,
+      GatewayIntentBits.MessageContent,
     ],
     partials: [Partials.Message, Partials.Channel, Partials.Reaction],
     allowedMentions: { parse: ['users'], repliedUser: false },
-});
-client.commands = new Collection();
-client.aliases = new Collection();
-client.limits = new Collection();
-client.queue = new Collection();
-await dbConnect();
-require('./utils/passport.js').run(client);
-require('./utils/handlers.js').run(client);
-process.on('unhandledRejection', function (err) {
-    throw err;
-});
+  });
 
-process.on('uncaughtException', function (err) {
-   console.log(err);
-});
+  client.commands = new Collection();
+  client.aliases = new Collection();
+  client.limits = new Collection();
+  client.queue = new Collection();
 
-client.login().catch(err => console.error("Client Login Error: " + err.message));
+  await dbConnect();
+
+  require('./utils/passport.js').run(client);
+  require('./utils/handlers.js').run(client);
+  
+  process.on('unhandledRejection', (err) => {
+    console.error('Unhandled Rejection:', err);
+    process.exit(1);
+    });
+  process.on('uncaughtException', (err) => {
+    console.error('Uncaught Exception:', err);
+    process.exit(1);
+    });
+
+  client.login().catch(err => console.error("Client Login Error: " + err.message));
+}
+
+start(); // Calling the async function
