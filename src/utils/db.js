7402180850d70849c14afab0c5f3861abeb36ce9
Theangel256 <angel.amarillas@proton.me>
2025-05-13 19:56:25 -0700

    Simplify MongoDB connection by removing unnecessary options in dbConnect, update mongouri

--- src/utils/db.js
+++ src/utils/db.js
@@ -15,10 +15,8 @@ async function dbConnect() {
   if (cached.conn) return cached.conn;
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then((mongoose) => {
+    cached.promise = mongoose.connect(MONGO_URI)
+    .then((mongoose) => {
       console.log("✅ Conectado a MongoDB");
       return mongoose;
     }).catch(err => {
