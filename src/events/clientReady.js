ff27a341e4680668a3b56e0ac8aa210c4bbfd9df
Theangel256 <angel.amarillas@proton.me>
2025-11-08 18:11:41 -0700

    fix url path from bot statues

--- src/events/clientReady.js
+++ src/events/clientReady.js
@@ -15,8 +15,11 @@ module.exports = async (client) => {
       .reduce((acc, guildCount) => acc + guildCount, 0)
       .toLocaleString()} users!`,
     "Theangel256 Studios V" + require("../../package.json").version,
-    `${process.env.PUBLIC_URL}/discord"`,
-    `${process.env.PUBLIC_URL}/add"`,
+    `https://theangel256.dev`,
+    `${substr(process.env.PUBLIC_URL, -8)}/discord"`,
+    `${substr(process.env.PUBLIC_URL, -8)}/add"`,
+    `${substr(process.env.PUBLIC_URL, -8)}/support"`,
+    `Serving ${client.guilds.cache.size.toLocaleString()} servers!`,
   ];
   setInterval(() => {
   const status = statues[Math.floor(Math.random() * statues.length)];
